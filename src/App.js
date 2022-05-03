faa286ede75dd51d0022f8f66244b78a051cc0dc
Sahil Bakshi <76874427+sahil-codee@users.noreply.github.com>
2022-05-03 21:37:59 +0530

    Update App

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ function App() {
   return (
     <div className="App">
       <h1>Lets build the facebook clone.</h1>
+
+
+
+      {/* Header */}
+
+      {/* App Body */}
+      {/* Sidebar */}
+      {/* Feed */}
+      {/* Widgets */}
     </div>
   );
 }
