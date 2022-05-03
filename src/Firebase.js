7b05f5325aeed6c524b7d71e1ea91049482fa96a
Sahil Bakshi <76874427+sahil-codee@users.noreply.github.com>
2022-05-03 21:32:12 +0530

    Facebook-clone

---
+++ src/Firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDgVZUui-ZeP2q_1wDac24Ugv7c9asn2S0",
+    authDomain: "facebook-clone-2e8ed.firebaseapp.com",
+    projectId: "facebook-clone-2e8ed",
+    storageBucket: "facebook-clone-2e8ed.appspot.com",
+    messagingSenderId: "49105802113",
+    appId: "1:49105802113:web:6d37c4d0f23e79d36f8e80",
+    measurementId: "G-9BD89KGN1G"
+  };
